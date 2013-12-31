491cd745c97e6e74ea48cace7b7cb9d9df786a19
Jordan Boyd-Graber <jbg@context00.umiacs.umd.edu>
2013-12-31 09:36:41 -0500

    Fixing resource
    Former-commit-id: 51349c16e47574888cadf18f72742cf891152894

--- python/site.py
+++ python/site.py
@@ -5,7 +5,7 @@ from website_writer import WebsiteWriter
 home = os.environ.get('HOME')
 print "Home: ", home
 
-writer = WebsiteWriter("src/", home + "/public_html/", "boilerplate/_header.html", "boilerplate/_footer.html",
+writer = WebsiteWriter("src/", home + "/public_html/", "resources/_header.html", "resources/_footer.html",
                        "Jordan Boyd-Graber", "http://umiacs.umd.edu/~jbg/")
 writer.add_index("pubs/", "Publications", [("Year", 0, []), ("Category", 0, []), ("Authors", 1, ["Jordan Boyd-Graber"]), ("Venue", 0, [])])
 writer.write()
