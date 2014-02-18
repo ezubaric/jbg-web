55d0c1481ea1302092be5a0eace3f840cf189eab
Jordan Boyd-Graber <jbg@context01.umiacs.umd.edu>
2014-02-17 21:49:42 -0500

    sol 4
    Former-commit-id: c3556b1b0c8d631e55805751b9fcc2f04c17fe4f [formerly 13baf19531f2fb750e369d3e06ec318d6647f8b4]
    Former-commit-id: 9415760f550c8965203f14c407ab782243fd1674

--- teaching/DATA_DIGGING/solution_04.R
+++ teaching/DATA_DIGGING/solution_04.R
@@ -7,19 +7,25 @@ colnames(mtcars)[1:2] <- c("dummy1", "dummy2")
 
 # Creating a training set
 
- target <- as.matrix(mtcars$mpg)
+target <- as.matrix(mtcars$mpg)
 features <- as.matrix(subset(mtcars, select=-c(mpg)))
 
 library(glmnet)
 reg.l2 <- glmnet(features, target, alpha=0)
 reg.l1 <- glmnet(features, target, alpha=1)
-models <- data.frame(t(rbind(matrix(reg.l2$lambda, nrow=1), as.matrix(reg.l2$beta))))
-colnames(models)[1] <- "lambda"
-models <- melt(models, c("lambda"))
-ggplot(m) + aes(x=log(lambda), y=value, color=variable) + geom_line()
+models.l2 <- data.frame(t(rbind(matrix(reg.l2$lambda, nrow=1), as.matrix(reg.l2$beta))))
+colnames(models.l2)[1] <- "lambda"
+models.l2 <- melt(models, c("lambda"))
+coef_l1 <- ggplot(models.l2) + aes(x=log(lambda), y=value, color=variable) + geom_line()
+
+models.l1 <- data.frame(t(rbind(matrix(reg.l1$lambda, nrow=1), as.matrix(reg.l1$beta))))
+colnames(models.l1)[1] <- "lambda"
+models.l1 <- melt(models, c("lambda"))
+coef_l2 <- ggplot(models.l2) + aes(x=log(lambda), y=value, color=variable) + geom_line()
 
 # Cross validation resampling the training data for L2 regression and L1 regression
 cv.l2 <- cv.glmnet(features, target, alpha=0)
 cv.l1 <- cv.glmnet(features, target, alpha=1) plot(reglm.l1, main = "L1 n-fold cross validation error")
 plot(reglm.l2, main = "L2 n-fold cross validation error") 
+plot(reglm.l1, main = "L1 n-fold cross validation error") 
 
