8492722be1f388e85ccdd71c14b29430e9fa1c3b
Jordan Boyd-Graber <jordanbg@gmail.com>
2014-08-26 06:47:28 -0600

    new site layout
    Former-commit-id: 332bc516be61855bb466337b60503710df03d4f0 [formerly 332bc516be61855bb466337b60503710df03d4f0 [formerly 332bc516be61855bb466337b60503710df03d4f0 [formerly 431fc75f2aa7aeb097c1ed8099d0bcd895aa6a14]]] [formerly 7f112246e96c948cf3fb9fb1fdc3412d03b1f58a] [formerly ddb1b1147fc96e27252be5b2da6b80c6870f8fa5 [formerly ddb1b1147fc96e27252be5b2da6b80c6870f8fa5 [formerly ddb1b1147fc96e27252be5b2da6b80c6870f8fa5 [formerly 744f8d38892edb47ed16d3d8b76e6b2bc417d451]]] [formerly 7f112246e96c948cf3fb9fb1fdc3412d03b1f58a [formerly 7f112246e96c948cf3fb9fb1fdc3412d03b1f58a [formerly 7f112246e96c948cf3fb9fb1fdc3412d03b1f58a [formerly 76daa6b97353702f21e34c04daaf39112705d458]]]]]
    Former-commit-id: 9a52cf1cc4afde437a8707f9ddbad6a132d4fabf [formerly 8244789240f976b8748a98760b4515cea0a16e29] [formerly 9a52cf1cc4afde437a8707f9ddbad6a132d4fabf [formerly 8244789240f976b8748a98760b4515cea0a16e29] [formerly 8244789240f976b8748a98760b4515cea0a16e29 [formerly 16ebd51df2838e0dbe7ecd69d1c06673d22562ea]]] [formerly e0b331983881787048299057b62d5dd68dc15031 [formerly e0b331983881787048299057b62d5dd68dc15031 [formerly e0b331983881787048299057b62d5dd68dc15031 [formerly 22f60cfc03f9e5128aaeceb96ae76b6b2662bf78]]]]
    Former-commit-id: e0b331983881787048299057b62d5dd68dc15031
    Former-commit-id: 703250a1be2b45d85ed05d2ff8c82f464c2e3ac9 [formerly 6f4c0c200bb5aafbe4796ff3b6f71801304c654a] [formerly 9077252f1d7cb6f2c5316aa4d7c19b2bd9ad7fbd [formerly 8c4c1739014ae91231a755785b467224d0b8f751]]
    Former-commit-id: ad3fdc13b85b6f80de9b498fc1fd48fd42155fc4 [formerly c76e2564a32b6b77c050073ea9ff6d7d70372184]
    Former-commit-id: 6e07ddc2c71a8290304caa118ecc83d36582eb8b

--- deploy.sh
+++ deploy.sh
@@ -6,25 +6,11 @@ git checkout $(git ls-files -d)
 python python/site.py
 
 CHANGES=`git whatchanged --since="3 days ago" -p pubs/`
-if [ ${#CHANGES} -gt 0 ]
-   then
-        for FILE in `ls pubs/*.tex`
-        do
-            pdflatex $FILE
-        done
-fi
 
 rm python/*.pyc
 rm pubs/*.tex *.aux *.log
-mv *.pdf docs
 
 cp style.css ~/public_html/
-
-for SUBDIR in docs images downloads teaching qb projects style
-        do
-            mkdir -p ~/public_html/$SUBDIR
-            cp $SUBDIR/*.* ~/public_html/$SUBDIR
-done
 
 rm -rf ~/public_html/teaching/*
 for CLASS in LBSC_690_2012 INFM_718_2011 COS_280_2008 CMSC_773_2012 DATA_DIGGING CMSC_723_2013 CSCI_5832 DEEP
@@ -41,5 +27,19 @@ for CLASS in LBSC_690_2012 INFM_718_2011 COS_280_2008 CMSC_773_2012 DATA_DIGGING
 	   done
 done
 
+if [ ${#CHANGES} -gt 0 ]
+   then
+        for FILE in `ls pubs/*.tex`
+        do
+            pdflatex $FILE
+        done
+fi
+mv *.pdf docs
+
+for SUBDIR in docs images downloads teaching qb projects style
+        do
+            mkdir -p ~/public_html/$SUBDIR
+            cp $SUBDIR/*.* ~/public_html/$SUBDIR
+done
 
 rm pubs/*.*
