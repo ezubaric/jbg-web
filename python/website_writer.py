457897c68fd06271b6039a2e255625ddf9fc6e08
Jordan Boyd-Graber <jordanbg@gmail.com>
2014-08-26 18:35:23 -0600

    hal name (partial support) and new lecture
    Former-commit-id: 9def9595315e25d80ce762e1389f3b209ac42add [formerly 9def9595315e25d80ce762e1389f3b209ac42add [formerly 9def9595315e25d80ce762e1389f3b209ac42add [formerly 113ae3cc4579a28227d0e2c5c41253df8ac9cbf1]]] [formerly 64fd57b227354156300f45d7b0323174a71ee318] [formerly 0fd7d18992e93d9b65b0aae0e01fdf4a84206e0b [formerly 0fd7d18992e93d9b65b0aae0e01fdf4a84206e0b [formerly 0fd7d18992e93d9b65b0aae0e01fdf4a84206e0b [formerly 467fd4c909c434b3b9cf9196ffb6857720edceb6]]] [formerly 64fd57b227354156300f45d7b0323174a71ee318 [formerly 64fd57b227354156300f45d7b0323174a71ee318 [formerly 64fd57b227354156300f45d7b0323174a71ee318 [formerly 8018903cc133dd023a7e63a4298e17be79e46924]]]]]
    Former-commit-id: 085758198dbb80cbe49f15d6436f6796560cc58e [formerly 085758198dbb80cbe49f15d6436f6796560cc58e [formerly 085758198dbb80cbe49f15d6436f6796560cc58e [formerly 078548b3a65c36d3b99474cc15314cd3147e35b7]]] [formerly 38e071bda4886b8630b4bd4b34896371e060fb44 [formerly 38e071bda4886b8630b4bd4b34896371e060fb44 [formerly 38e071bda4886b8630b4bd4b34896371e060fb44 [formerly 1d0b6c7b1335b12af9678db5bd016f4a72b6c933]]]]
    Former-commit-id: 38e071bda4886b8630b4bd4b34896371e060fb44
    Former-commit-id: 686c4a70b5398b14147ea1e722fe170153ede077 [formerly 9d5aa3dcf2f8edc064cf252fe26b86374be3e366] [formerly 6e265b5dad0f6bd62570d3cc728d5187cccef8b0 [formerly a0acfb29ac2ba20b931308cd1fd7c69297223137]]
    Former-commit-id: 937b2f449ff1b67d1ebc14ea248e7c1cfa0bff92 [formerly 68c95d4c13fcdad9af619c9e79e9d33881ff6338]
    Former-commit-id: 4977253ddaeff6927f210cf7b3149714dbc30474

--- python/website_writer.py
+++ python/website_writer.py
@@ -25,6 +25,7 @@ STUDENTS = {"Ke Zhai": Student("Ke Zhai", 2010, 2015, "http://www.umiacs.umd.edu
             "Mohamad Alkhouja": Student("Mohamad Alkhouja", 2011, 2013),
             "Thang Nguyen": Student("Thang Nguyen", 2014, 2019, "http://www.umiacs.umd.edu/~daithang/"),
             "Mohit Iyyer": Student("Mohit Iyyer", 2014, 2019, "http://cs.umd.edu/~miyyer/"),
+            "Alvin Grissom II": Student("Avlin Grissom II", 2013, 2017, "http://www.umiacs.umd.edu/~alvin/"),
             "Eric Hardisty": Student("Eric Hardisty", 2010, 2011)}
 
 def format_name(students, name, year, latex):
@@ -51,7 +52,9 @@ def format_name(students, name, year, latex):
       return "<b>Jordan Boyd-Graber</b>"
 
   if not latex:
-    name = name.replace("\'e", "&eacute;")
+    name = name.replace("\\'{e}", "&eacute;")
+    name = name.replace("\\'e", "&eacute;")    
+  print(name)
   
   return name
 
