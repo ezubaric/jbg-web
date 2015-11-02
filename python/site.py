64f661633cfad3be1080ccecf53839bc449871da
Jordan Boyd-Graber <jordanbg@gmail.com>
2015-11-02 09:16:19 -0700

    autogenerate project publications
    Former-commit-id: 6813ce05e742edcf7fd15b7411dbabec36a04c35

--- python/site.py
+++ python/site.py
@@ -1,14 +1,16 @@
 
 import os
 import sys
-from website_writer import WebsiteWriter
+from website_writer import WebsiteWriter, global_replace
 
 home = os.environ.get('HOME')
 print "Home: ", home
 
 writer = WebsiteWriter("src/", home + "/public_html/", "resources/_header.html", "resources/_footer.html",
                        "Jordan Boyd-Graber", "http://cs.colorado.edu/~jbg/", sys.argv[1])
-writer.add_index("pubs/", "Pubs", [("Year", 0, []), ("Category", 0, []), ("Authors", 1, ["Jordan Boyd-Graber"]), ("Venue", 0, [])])
+writer.add_index("pubs/", "Pubs", [("Year", 0, []), ("Category", 0, []), ("Authors", 1, ["Jordan Boyd-Graber"]), ("Venue", 0, []), ("Project", 0, [])])
 writer.write()
-
 writer.write_index("Pubs", True)
+print("----------------------")
+writer.add_raw("teaching/*/index.html", "../../")
+writer.add_raw("projects/*.html", "../")
