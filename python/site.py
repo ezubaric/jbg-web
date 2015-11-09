f60c9712d332adba0d62e78175a2cfa6f7a703b3
Jordan Boyd-Graber <jordanbg@gmail.com>
2015-11-09 10:22:01 -0700

    adding more media
    Former-commit-id: bb1ba522fb82d7d25f67f6654f15dc67a67d1054

--- python/site.py
+++ python/site.py
@@ -9,8 +9,11 @@ print "Home: ", home
 writer = WebsiteWriter("src/", home + "/public_html/", "resources/_header.html", "resources/_footer.html",
                        "Jordan Boyd-Graber", "http://cs.colorado.edu/~jbg/", sys.argv[1])
 writer.add_index("pubs/", "Pubs", [("Year", 0, []), ("Category", 0, []), ("Authors", 1, ["Jordan Boyd-Graber"]), ("Venue", 0, []), ("Project", 0, [])])
+writer.add_index("media/", "Media", [("Year", 0, []), ("Category", 0, []), ("Project", 0, [])])
+
 writer.write()
 writer.write_index("Pubs", True)
+writer.write_index("Media", True)
 print("----------------------")
 writer.add_raw("teaching/*/index.html", "../../")
 writer.add_raw("projects/*.html", "../")
