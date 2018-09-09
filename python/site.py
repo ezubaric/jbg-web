61b75262a6e5cf21a972a906a09f10558b70e3d7
Jordan Boyd-Graber <jordanbg@gmail.com>
2018-09-09 15:01:57 -0400

    pubs

--- python/site.py
+++ python/site.py
@@ -7,7 +7,7 @@ home = os.environ.get('HOME')
 print("Home: " + home)
 
 writer = WebsiteWriter("src/", home + "/public_html/", "resources/_header.html", "resources/_footer.html",
-                       "Jordan Boyd-Graber", "http://cs.colorado.edu/~jbg/", sys.argv[1])
+                       "Jordan Boyd-Graber", "http://umiacs.umd.edu/~jbg/", sys.argv[1])
 writer.add_index("pubs/", "Pubs", [("Year", 0, []), ("Category", 0, []), ("Authors", 1, ["Jordan Boyd-Graber"]), ("Venue", 0, []), ("Project", 0, [])], "Year")
 writer.add_index("media/", "Media", [("Year", 0, []), ("Category", 0, []), ("Project", 0, [])], "Category")
 
