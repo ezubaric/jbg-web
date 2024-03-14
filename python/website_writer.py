a77346d69452c016222439679324a1ea4ac95542
Jordan Ying <jbg@MacBook-Pro-von-Jordan.local>
2024-03-14 15:36:13 -0500

    new publications
    Former-commit-id: 94230d4e81ad42b6f815cedeb2a9acad693f32ba

--- python/website_writer.py
+++ python/website_writer.py
@@ -76,6 +76,7 @@ UMD_MAPPING = {"Chapter": "\\ifumd II.B.1. \else \\fi Chapters in Books",
 
 kSTUDENTS = {"Kyle Seelman": Student("Kyle Seelman", 2022, 2026),
              "HyoJung Han": Student("HyoJung Han", 2022, 2027, "https://h-j-han.github.io/"),
+             "Tin Nguyen": Student("Tin Nguyen", 2022, 2023),
              "Wichayaporn Wongkamjan": Student("Wichayaporn Wongkamjan", 2022, 2026),
              "Ke Zhai": Student("Ke Zhai", 2010, 2014, "https://kzhai.github.io/",
                                 job="Microsoft, Senior Research Scientist"),
