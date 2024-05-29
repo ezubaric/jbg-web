5f2638eaa43c1482a251c4012d7e56ee1962235e
Jordan Boyd-Graber <jordanbg@gmail.com>
2024-05-28 20:54:00 -0400

    code generates dates but not HTML

--- python/class_webpage.py
+++ python/class_webpage.py
@@ -1,6 +1,9 @@
 
 from datetime import datetime
 import pandas as pd
+
+from collections import defaultdict
+
 import json
 
 class Course:
@@ -10,7 +13,7 @@ class Course:
     def load_holidays(self, path):
         with open(path, 'r') as infile:
             self._noclass = json.loads(infile.read())
-        self._noclass = set(datetime.strptime(x, "%Y-%m-%d").date() for x in self._noclass)
+        self._noclass = dict((datetime.strptime(x[0], "%Y-%m-%d").date(), x[1]) for x in self._noclass)
         
     def load_json(self, path):
         with open(path, 'r') as infile:
@@ -26,31 +29,61 @@ class Course:
         self.class_days = raw["config"]["days"]
         self.hw_day = raw["config"]["hw_day"]
 
+    @staticmethod
+    def date_range(start, stop, day):
+        days = list(pd.date_range(start, end=stop, freq=day))
+        return [x.to_pydatetime().date() for x in days]
+        
     def class_dates(self):
         instruction_days = []
+
         for day in self.class_days:
-            instruction_days += list(pd.date_range(self.start, end=self.end, freq=day))
+            instruction_days += self.date_range(self.start, self.end, day)
 
+        print(instruction_days, self._noclass)
         noclass = [x for x in instruction_days if x in self._noclass]
+        print("NOCLASS", noclass)
 
-        homework_days = list(pd.date_range(self.start, end=self.end, freq=self.hw_day))
+        homework_days = self.date_range(self.start, self.end, self.hw_day)
         homework_days = [x for x in homework_days if x not in self._noclass]
 
         days = [(x, "INST") for x in instruction_days] + \
                [(x, "HOL") for x in noclass] + \
-               [(x, "HW") for x in hw]
-
-               
-        
+               [(x, "HW") for x in homework_days]
+            
         days.sort()
-        return days
-        
-    def render(self, class_template, hw_template):
-        html = ""
-        for day, topic in zip(self.class_dates(), self._sequence):
-            html += ""
-        
 
+        day_lookup = defaultdict(dict)
+
+        for day, homework in zip(homework_days, self._hw_sequence):
+            if homework is not None:
+                day_lookup[day]["HW"] = homework
+
+        for day, lecture_material in zip(instruction_days, self._class_sequence):
+            day_lookup[day]["INST"] = lecture_material
+
+        for day in noclass:
+            day_lookup[day]["HOL"] = self._noclass[day]
+        
+        return day_lookup
+        
+    def render(self, class_template, hw_template, hol_template):
+        html = ""
+
+        days = self.class_dates()
+        
+        day_keys = days.keys()
+        day_keys.sort()
+
+        for day in day_keys:
+            for element_type in days:
+                if element_type == "INST":
+                    html += ""
+                elif element_type == "HW":
+                    html += ""
+                elif element_type == "HOL":
+                    html += ""
+        return html
 
 if __name__ == "__main__":
     c = Course()
